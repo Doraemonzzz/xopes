5403e95b16911d427664424988ebd9506fc1cedb
Doraemonzzz <715073608@qq.com>
2024-09-07 06:28:08 +0000

    [Feature Add] add support for flattened input for md_lrpe

--- xopes/ops/md_lrpe/cosine/md_lrpe_cosine_torch.py
+++ xopes/ops/md_lrpe/cosine/md_lrpe_cosine_torch.py
@@ -1,5 +1,5 @@
 import torch
-
+from einops import pack
 
 def md_lrpe_cosine_torch(x, theta, shape=None):
     # x: b, h, ..., d
@@ -27,9 +27,41 @@ def md_lrpe_cosine_torch(x, theta, shape=None):
 
     theta = torch.cat(theta_list, dim=-1)[..., :d]
 
+    # when x is flatten, we need to pack theta
+    if len(x.shape) == 4: # b h n d
+        theta, ps = pack([theta], 'h * d')
+
     cos = theta.cos()
     sin = theta.sin()
 
     output = torch.cat([x * cos, x * sin], dim=-1)
 
     return output.to(x.dtype)
+
+if __name__ == "__main__":
+    # unit test
+    from xopes.utils import next_power_of_two
+    
+    def test(use_pack=False):
+        shape = tuple([2, 8, 32, 32, 64])
+        h = shape[1]
+        d = shape[-1]
+        m = len(shape) - 3
+        e = next_power_of_two((d + m - 1) // m)
+        dtype = torch.float32
+        device = torch.cuda.current_device()
+        x = (torch.randn(shape, dtype=dtype, device=device)).requires_grad_()
+        if use_pack:
+            x, ps = pack([x], 'b h * d')
+
+        theta = torch.randn((h, e), dtype=dtype, device=device)
+        shape = shape[:-1] + (shape[-1] * 2,)
+        do = torch.randn(shape, dtype=dtype, device=device)
+        if use_pack:
+            do, ps = pack([do], 'b h * d')
+
+        o = md_lrpe_cosine_torch(x, theta, shape=shape[2:-1])
+        o.backward(do)
+        
+    test(False)
+    test(True)
