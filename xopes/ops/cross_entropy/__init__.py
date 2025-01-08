6bbaf57014b8f8e8ce9e779d492dbee1210a7ed9
Doraemonzzz <715073608@qq.com>
2025-01-08 15:03:47 +0000

    [Feature Add] add ce interface

--- xopes/ops/cross_entropy/__init__.py
+++ xopes/ops/cross_entropy/__init__.py
@@ -2,3 +2,5 @@ from .baseline import cross_entropy_fla_wrapper
 from .ce_parallel_triton import cross_entropy_parallel_triton
 from .ce_torch import cross_entropy_torch
 from .ce_triton import cross_entropy_triton
+
+cross_entropy_fn = cross_entropy_parallel_triton
