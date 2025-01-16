c987f74ac078c10a38cb028ebd344cea87d5fdf9
Doraemonzzz <715073608@qq.com>
2025-01-16 03:24:08 +0000

    [Feature Add] add lrpe interface

--- xopes/ops/lrpe/__init__.py
+++ xopes/ops/lrpe/__init__.py
@@ -1,15 +1,19 @@
 from .cosine import lrpe_cosine_bwd, lrpe_cosine_fn, lrpe_cosine_fwd
+from .rotate import lrpe_rotate_bwd, lrpe_rotate_fn, lrpe_rotate_fwd
 
 LRPE_DICT = {
     "cosine": lrpe_cosine_fn,
+    "rotate": lrpe_rotate_fn,
 }
 
 LRPE_FWD_DICT = {
     "cosine": lrpe_cosine_fwd,
+    "rotate": lrpe_rotate_fwd,
 }
 
 LRPE_BWD_DICT = {
     "cosine": lrpe_cosine_bwd,
+    "rotate": lrpe_rotate_bwd,
 }
 
 
@@ -18,12 +22,12 @@ def lrpe_fn(x, theta, offset=0, act="none", dim=None, lrpe_type="cosine", **kwar
     return fn(x=x, theta=theta, offset=offset, act=act, dim=dim)
 
 
-def lrpe_fwd(x, theta, offset=0, act="none", dim=None, lrpe_type="cosine", **kwargs):
+def lrpe_fwd_fn(x, theta, offset=0, act="none", dim=None, lrpe_type="cosine", **kwargs):
     fn = LRPE_FWD_DICT[lrpe_type]
     return fn(x=x, theta=theta, offset=offset, act=act, dim=dim)
 
 
-def lrpe_bwd(
+def lrpe_bwd_fn(
     x, theta, do, offset=0, act="none", dim=None, lrpe_type="cosine", **kwargs
 ):
     fn = LRPE_BWD_DICT[lrpe_type]
