1d63754ac9c04eb3d8e3e6a300e36c730675176d
Doraemonzzz <715073608@qq.com>
2025-02-15 15:53:08 +0000

    [Feature add] rm torch script since it's not compatible with dtensor

--- xopes/ops/act/_act_torch/act_torch.py
+++ xopes/ops/act/_act_torch/act_torch.py
@@ -2,25 +2,20 @@ import torch
 import torch.nn.functional as F
 
 
-
 def relu(x, dim=None):
     return F.relu(x)
-
 
 
 def relu_bwd(x, do, dim=None):
     return torch.where(x >= 0, do, 0)
 
 
-
 def grad_relu(x, dim=None):
     return torch.where(x >= 0, 1, 0)
 
 
-
 def sigmoid(x, dim=None):
     return F.sigmoid(x)
-
 
 
 def sigmoid_bwd(x, do, dim=None):
@@ -28,16 +23,13 @@ def sigmoid_bwd(x, do, dim=None):
     return do * sigmoid * (1 - sigmoid)
 
 
-
 def grad_sigmoid(x, dim=None):
     sigmoid = F.sigmoid(x)
     return sigmoid * (1 - sigmoid)
 
 
-
 def silu(x, dim=None):
     return F.silu(x)
-
 
 
 def silu_bwd(x, do, dim=None):
@@ -45,21 +37,17 @@ def silu_bwd(x, do, dim=None):
     return do * sigmoid * (1 + x * (1 - sigmoid))
 
 
-
 def grad_silu(x):
     sigmoid = F.sigmoid(x)
     return sigmoid * (1 + x * (1 - sigmoid))
-
 
 
 def none(x):
     return x
 
 
-
 def none_bwd(x, do):
     return do
-
 
 
 def grad_none(x):
