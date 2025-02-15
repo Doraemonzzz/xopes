72e44009fa72d2392526cb4521c0c29716769995
Doraemonzzz <715073608@qq.com>
2025-02-15 15:52:54 +0000

    [Feature add] rm torch script since it's not compatible with dtensor

--- xopes/ops/act/_act_torch/act_torch.py
+++ xopes/ops/act/_act_torch/act_torch.py
@@ -2,66 +2,66 @@ import torch
 import torch.nn.functional as F
 
 
-@torch.jit.script
+
 def relu(x, dim=None):
     return F.relu(x)
 
 
-@torch.jit.script
+
 def relu_bwd(x, do, dim=None):
     return torch.where(x >= 0, do, 0)
 
 
-@torch.jit.script
+
 def grad_relu(x, dim=None):
     return torch.where(x >= 0, 1, 0)
 
 
-@torch.jit.script
+
 def sigmoid(x, dim=None):
     return F.sigmoid(x)
 
 
-@torch.jit.script
+
 def sigmoid_bwd(x, do, dim=None):
     sigmoid = F.sigmoid(x)
     return do * sigmoid * (1 - sigmoid)
 
 
-@torch.jit.script
+
 def grad_sigmoid(x, dim=None):
     sigmoid = F.sigmoid(x)
     return sigmoid * (1 - sigmoid)
 
 
-@torch.jit.script
+
 def silu(x, dim=None):
     return F.silu(x)
 
 
-@torch.jit.script
+
 def silu_bwd(x, do, dim=None):
     sigmoid = F.sigmoid(x)
     return do * sigmoid * (1 + x * (1 - sigmoid))
 
 
-@torch.jit.script
+
 def grad_silu(x):
     sigmoid = F.sigmoid(x)
     return sigmoid * (1 + x * (1 - sigmoid))
 
 
-@torch.jit.script
+
 def none(x):
     return x
 
 
-@torch.jit.script
+
 def none_bwd(x, do):
     return do
 
 
-@torch.jit.script
+
 def grad_none(x):
     return 1
 
