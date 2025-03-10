5b1334d7a7566ed7adfb8e853de55fb19ede50e1
Doraemonzzz <715073608@qq.com>
2025-03-10 07:20:05 +0000

    [Feature add] add lape

---
+++ xopes/ops/lightning_attn/positional_encoding/lape_recurrence_triton.py
@@ -0,0 +1,64 @@
+from typing import Optional, Tuple
+
+import torch
+from einops import repeat
+
+from xopes.ops.lightning_attn.scalar_decay import lasd_recurrence_triton
+
+
+def lape_recurrence_triton(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    ld: Optional[torch.Tensor] = None,
+    initial_state: Optional[torch.Tensor] = None,
+    cu_seqlens: Optional[torch.LongTensor] = None,
+    **kwargs,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Apply Lightning Attention Recurrence with Scalar Decay in Triton.
+
+    Args:
+        q: Query tensor of shape (H, D)
+        k: Key tensor of shape (H, D)
+        v: Value tensor of shape (B, N, H, E)
+        ld: Logarithmic decay tensor of shape (H,)
+        initial_state: Initial state tensor of shape (B, H, D, E)
+        cu_seqlens: Cumulative sequence lengths tensor, this is used for varlen training
+
+    Returns:
+        output: Tensor of shape (B, N, H, E)
+        state: Tensor of shape (B, H, D, E)
+    """
+    b, n, h, e = v.shape
+    q.shape[-1]
+    q = repeat(q, "h d -> b n h d", b=b, n=n)
+    k = repeat(k, "h d -> b n h d", b=b, n=n)
+
+    return lasd_recurrence_triton(
+        q=q,
+        k=k,
+        v=v,
+        ld=ld,
+        initial_state=initial_state,
+        cu_seqlens=cu_seqlens,
+    )
+
+
+if __name__ == "__main__":
+    import torch.nn.functional as F
+
+    b, n, h, d = 2, 16, 12, 64
+    e = 32
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dtype = torch.bfloat16
+    q = torch.randn(h, d, device=device, dtype=dtype).requires_grad_(True)
+    k = torch.randn(h, d, device=device, dtype=dtype).requires_grad_(True)
+    v = torch.randn(b, n, h, e, device=device, dtype=dtype).requires_grad_(True)
+    ld = F.logsigmoid(torch.randn(h, device=device))
+    initial_state = torch.randn(b, h, d, e, device=device, dtype=dtype).requires_grad_(
+        True
+    )
+    output, final_state = lape_recurrence_triton(q, k, v, ld, initial_state)
+    loss = output.sum() + final_state.sum()
+    loss.backward()
---
+++ xopes/ops/lightning_attn/positional_encoding/lape_torch.py
@@ -0,0 +1,58 @@
+# lape: lightning attention positional encoding
+from typing import Optional, Tuple
+
+import torch
+import torch.nn.functional as F
+from einops import repeat
+
+from xopes.ops.lightning_attn.scalar_decay import lasd_torch
+
+
+def lape_torch(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    ld: Optional[torch.Tensor] = None,
+    initial_state: Optional[torch.Tensor] = None,
+    cu_seqlens: Optional[torch.LongTensor] = None,
+    **kwargs,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Apply Lightning Attention with Scalar Decay in Pytorch.
+
+    Args:
+        q: Query tensor of shape (H, D)
+        k: Key tensor of shape (H, D)
+        v: Value tensor of shape (B, N, H, E)
+        ld: Logarithmic decay tensor of shape (H,)
+        initial_state: Initial state tensor of shape (B, H, D, E)
+        cu_seqlens: Cumulative sequence lengths tensor, this is used for varlen training
+
+    Returns:
+        output: Tensor of shape (B, N, H, E)
+        state: Tensor of shape (B, H, D, E)
+    """
+    b, n, h, e = v.shape
+    q.shape[-1]
+    q = repeat(q, "h d -> b n h d", b=b, n=n)
+    k = repeat(k, "h d -> b n h d", b=b, n=n)
+
+    return lasd_torch(
+        q=q,
+        k=k,
+        v=v,
+        ld=ld,
+        initial_state=initial_state,
+        cu_seqlens=cu_seqlens,
+    )
+
+
+if __name__ == "__main__":
+    b, n, h, d = 2, 16, 12, 64
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dtype = torch.bfloat16
+    q = torch.randn(h, d, device=device, dtype=dtype)
+    k = torch.randn(h, d, device=device, dtype=dtype)
+    v = torch.randn(b, n, h, d, device=device, dtype=dtype)
+    ld = F.logsigmoid(torch.randn(h, device=device))
+    output, state = lape_torch(q, k, v, ld)
